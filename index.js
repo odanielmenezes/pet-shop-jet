6fc5d0fe3e6ca4316cc451516a8ab50054f48aaa
Daniel Menezes <daniel.menezes@infoar.com.br>
2022-12-21 19:34:02 -0300

    ajuste geral

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 var cartBtn = document.querySelector("#cart-button");
 var cartNumberItems = document.querySelectorAll(".cart-item");
+let modalKey = 0
+const select = (elemento) => document.querySelector(elemento)
+const seletAll = (elemento) => document.querySelectorAll(elemento)
+const cart = []
 
-document.querySelector("#menu-actions-number-items").innerHTML =
-  cartNumberItems.length;
+document.querySelector('#menu-actions-number-items').innerHTML =
+  document.querySelectorAll('#cart-item').length - 1;
 
 function openModal() {
   if (document.querySelector("#cart").classList.length > 1) {
@@ -18,7 +22,25 @@ function openModal() {
   }
 }
 
+var acaoBotao = function (index) {
+  return function(){
+   console.log('indexxxxxxxx', index) 
+  }
+};
+
+
 function btnComprar() {
-  console.log(cartNumberItems);
-  cartNumberItems.appendChild('AAAAAAAAA')
+
+    var botoes = document.getElementsByClassName("button-add-cart");
+    for (var i = 0; i < botoes.length; i++) {
+      botoes[i].addEventListener('click', acaoBotao(i), false);
+    }
+
+  let produtoItem = select('.cart .cart-items .cart-item').cloneNode(true)
+
+  select('.cart-items').append(produtoItem)
+  // select('.cart-item-img img').src = 'item.image'
+  select('.cart-item .cart-items-infos .cart-items-infos-name').innerHTML = 'item.name'
+  select('.cart-item .cart-items-infos .cart-items-infos-price').innerHTML = 'item.price'
 }
+
---
+++ produtos.js
@@ -0,0 +1,16 @@
+let produtosJson = [
+    {
+        id: 1,
+        image: './assets/product-2.png',
+        name: "Morbi Condimentum egestas quam - 3m",
+        price: "R$18,90",
+        oldPrice: "R$39,90"
+    },
+    {
+        id: 2,
+        image: './assets/product-2.png',
+        name: "Vivamus quam ex, ultricies et blandit sed",
+        price: "R$66,90",
+        oldPrice: "R$99,90"
+    }
+]
